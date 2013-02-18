0f8b044d067c7cdffd8ba01dbae0f7a6db0f5403
Matt Korostoff <MKorostoff@gmail.com>
2013-02-18 11:20:32 -0500

    adding mobile scaling to index.php

--- .pdirl/templates/default/index.php
+++ .pdirl/templates/default/index.php
@@ -2,6 +2,7 @@
 <html lang='en' xml:lang='en' xmlns='http://www.w3.org/1999/xhtml'>
 <head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title><?php echo _('Directory listing for '), host(), $_SERVER['REQUEST_URI']; ?></title>
 		<link rel="stylesheet" type="text/css" href="<?php echo templateDirectory();?>/style.css" />
 	</head>
